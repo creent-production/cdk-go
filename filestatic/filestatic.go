ef1dd79830f736ccfbbf2c71277f1eed16268f1d
IndominusByte <nyomanpradipta120@gmail.com>
2022-02-07 19:13:47 +0800

    add serve file static

---
+++ filestatic/filestatic.go
@@ -0,0 +1,27 @@
+package filestatic
+
+import "net/http"
+
+// FileSystem custom file system handler
+type FileSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file
+func (fs FileSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		return fs.fs.Open("upps!")
+	}
+
+	return f, nil
+}
