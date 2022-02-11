427988fa5c71bab8ed727c3b39e6e9c599ffe084
IndominusByte <nyomanpradipta120@gmail.com>
2022-02-11 13:59:09 +0800

    add function parser

--- parser/parser.go
+++ parser/parser.go
@@ -17,3 +17,28 @@ func ParsePathToInt(regex, path string) (int, error) {
 	}
 	return result, nil
 }
+
+func ParseSliceIntToSliceStr(values []int) []string {
+	valuesText := []string{}
+
+	for i := range values {
+		text := strconv.Itoa(values[i])
+		valuesText = append(valuesText, text)
+	}
+
+	return valuesText
+}
+
+func ParseSliceStrToSliceInt(values []string) ([]int, error) {
+	valuesInt := []int{}
+
+	for i := range values {
+		integer, err := strconv.Atoi(values[i])
+		if err != nil {
+			return []int{}, err
+		}
+		valuesInt = append(valuesInt, integer)
+	}
+
+	return valuesInt, nil
+}
