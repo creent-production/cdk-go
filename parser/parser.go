24dfe277fc22d91140fdca0cf8b083eb85ab9bed
IndominusByte <nyomanpradipta120@gmail.com>
2022-07-11 20:43:45 +0800

    add remove duplicate str/int

--- parser/parser.go
+++ parser/parser.go
@@ -154,3 +154,27 @@ func DifferenceIntFoundOrNot(a, b []int, f bool) []int {
 	}
 	return diff
 }
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func removeDuplicateInt(intSlice []int) []int {
+	allKeys := make(map[int]bool)
+	list := []int{}
+	for _, item := range intSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
